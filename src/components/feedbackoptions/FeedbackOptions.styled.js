e9ccd3f9690eee5210cc2effcf5be7c4c1b6b029
Veronika Savluk <s_veronica@ukr.net>
2022-09-24 00:01:17 +0300

    Create FeedbackOptions.styled.js

---
+++ src/components/feedbackoptions/FeedbackOptions.styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Options = styled.div`
+    display: flex;
+    margin: 0;
+    padding: 0;
+    aling-item: center;
+    `;
+
+// export const Option = styled.li`
+// column-gap: 15px;
+// `;
+export const Btn = styled.button`
+    display: block;font-size: 1em;
+    margin: 0;
+    padding: 4px;
+    border: 1px solid #6495ED;
+    border-radius: 5px;
+    background: #6495ED;
+    color: white;
+`;
+
+    // background: ${ props => props.primary ? "#6495ED" : "white" };
+    // color: ${props => props.primary ? "white" : "#6495ED"};

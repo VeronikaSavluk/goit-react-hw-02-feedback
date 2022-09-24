4e5846ee32ae589fbc1f193254797e1e9eb7c69b
Veronika Savluk <s_veronica@ukr.net>
2022-09-25 02:05:40 +0300

    Update FeedbackOptions.js

--- src/components/feedbackoptions/FeedbackOptions.js
+++ src/components/feedbackoptions/FeedbackOptions.js
@@ -1,17 +1,31 @@
+import PropTypes from 'prop-types';
+
 import {
     Options,
     Btn,
 } from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+    const buttons = Object.keys(options);
 
     return (
         <Options>
-            <Btn type="button" onClick={onLeaveFeedback}>Good</Btn>
-            <Btn type="button" onClick={onLeaveFeedback}>Neutral</Btn>
-            <Btn type="button" onClick={onLeaveFeedback}>Bad</Btn>
+            {buttons.map(button => (
+                <Btn type="button" onClick={onLeaveFeedback} key={button}>{button}</Btn>
+            ))}
         </Options>
     )
 };
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.PropTypes.objectOf(PropTypes.number),
+    buttons: PropTypes.PropTypes.arrayOf(
+        PropTypes.exact({
+            good: PropTypes.string.isRequired,
+            neutral: PropTypes.string.isRequired,
+            bad: PropTypes.string.isRequired,
+  })),
+    onLeaveFeedback: PropTypes.func.isRequired,
+}
 
 export default FeedbackOptions;
